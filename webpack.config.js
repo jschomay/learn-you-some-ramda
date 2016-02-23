3e257820142106925afb16015d19024ea2331dd0
Jeff Schomay <jschomay@gmail.com>
2016-02-23 08:01:57 -0700

    make it hot!

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,21 @@
 module.exports = {
-    entry: "./main.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    devtool: 'source-map',
-    module: {
-        loaders: [
-          {
-            test: /\.js?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel', // 'babel-loader' is also a legal name to reference
-          }
-        ]
-    }
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    "./main.js",
+  ],
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+      }
+    ]
+  }
 };
